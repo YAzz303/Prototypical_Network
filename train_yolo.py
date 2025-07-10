a613b6596130d59de70f58e6057509c6d866dcc8
YAzz303 <tuladharyajjyu.best@gmail.com>
2025-07-10 23:07:30 +0545

    tested

--- train_yolo.py
+++ train_yolo.py
@@ -2,19 +2,19 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Load a YOLOv8 model (nano version for speed, or use yolov8s.pt, yolov8m.pt, etc.)
-    model = YOLO('yolov8s.pt')
+    model = YOLO('yolov8n.pt')
 
     # Train the model
     model.train(
         data='manuscript.yaml',
         epochs=100,
-        imgsz=416,  # Image size for training
-        workers=2,
+        imgsz=320,  # Image size for training
+        workers=1,
         batch=8,
-        name='yolov8s-3',
+        name='yolov8n-detector',
         pretrained=True,
-        single_cls=True,  # Set to True if you have only one class
-        save_period=10,  # Save model every 10 epochs
-        save=True,  # Save the model after training
-        device=0,  # Use GPU 0, change to -1 for CPU
+        single_cls=True,  
+        save_period=10, 
+        save=True,  
+        device=0,  
     )
